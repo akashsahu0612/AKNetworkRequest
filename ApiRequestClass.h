a1483de48cc7573927f65b6c9c9118e836eb7130
akashsahu0612 <39698236+akashsahu0612@users.noreply.github.com>
2018-05-28 20:16:02 +0530

    Initial Commit. Supports GET and POST method

--- ApiRequestClass.h
+++ ApiRequestClass.h
@@ -1,7 +1,7 @@
 //  ApiRequestClass.h
 //
 //  Created by Anand Kumar Singh on 01/01/18.
-//  Copyright © 2018 Vyrazu Labs Pvt Ltd. All rights reserved.
+//  Copyright ©2018 Vyrazu Labs Pvt Ltd. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
